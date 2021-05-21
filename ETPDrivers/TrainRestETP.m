6f69acd4e3f331ef007bc7bc0f0e28801aec68b2
briank621 <bck2116@columbia.edu>
2021-05-21 16:08:39 -0400

    Add comments

--- ETPDrivers/TrainRestETP.m
+++ ETPDrivers/TrainRestETP.m
@@ -13,25 +13,19 @@ datasetName = 'PVT';
 inputFolder = strcat(pwd, '/../Datasets/', datasetName, '/Rest/CellData/');
 
 % Configs
-
-% Change Sampling Rate and electrodes
-samplingRate = 250;
 targetFreq = [8 13];
 targetChannel = "Oz";
 neighbors = ["O1", "O2", "POz", "PO3", "PO4"];
-
-% Look at the channel locations for PVT_S11_D9_REST_CELLDATA.mat
-% Get the results for the later 500 ms
 
 files = dir(inputFolder);
 allPhases = [];
 for i = 1:length(files)
 	fileName = files(i).name;
-	if(endsWith(fileName, 'CELLDATA500.mat'))
+	if(endsWith(fileName, 'CELLDATA500.mat')) % Change this to be CELLDATA1000.mat for 1000ms
 		filePath = strcat(inputFolder, fileName);
 		EEG = load(filePath);
         electrodes = ExtractElectrodes(EEG.chanlocs, targetChannel, neighbors);
-		[cycleEstimate, actualPhases, actualPowers] = ETPTrainEpoch(EEG.data, 'TargetFreq', targetFreq, 'Electrodes', electrodes, 'SamplingRate', samplingRate, ...
+		[cycleEstimate, actualPhases, actualPowers] = ETPTrainEpoch(EEG.data, 'TargetFreq', targetFreq, 'Electrodes', electrodes, 'SamplingRate', EEG.srate, ...
             'Graph', false);
 		allPhases = [allPhases actualPhases];
 	end
