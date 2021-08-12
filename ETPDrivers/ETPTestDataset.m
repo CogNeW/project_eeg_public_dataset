c2e00b25be249dd9d87c12eb98597af9a494dcf1
BE_Battlestation <be_battlestation@Brians-iMac.fios-router.home>
2021-08-12 10:15:04 -0400

    Updated tests to include all datasets

--- ETPDrivers/ETPTestDataset.m
+++ ETPDrivers/ETPTestDataset.m
@@ -11,53 +11,66 @@
 %   Find the appropriate one in test
 %   Run test epoch, and get the predicted angles
 
+addpath(strcat(pwd, '/../ETPAlgorithm/dependencies/fieldtrip-20201214'));
 addpath(strcat(pwd, '/../ETPAlgorithm/utilities'));
 addpath(strcat(pwd, '/../ETPAlgorithm'));
 
-datasetName = 'PVT_preprocessed';
+datasetNames = ["PVT" "ALPH" "B3" "COV" "AB"];
 
-restIPIFolder = strcat(pwd, '/../datasets/open_source_c_epoched/', datasetName, '/outputs/ETPTrain/pseudorest/500/');
-taskFolder = strcat(pwd, '/../datasets/open_source_c_epoched/', datasetName, '/not_chan_reduced/task/mat/');
-outputFolder = strcat(pwd, '/../datasets/open_source_c_epoched/', datasetName, '/outputs/ETPTest/task/500/');
+for datasetIndex = 1:length(datasetNames)
+    
+    datasetName = datasetNames(datasetIndex);
+    
+    taskFolder = strcat(pwd, '/../datasets/open_source_c_epoched/', datasetName, '/not_chan_reduced/task/mat/');
+    restIPIFolder = strcat(pwd, '/../datasets/open_source_d_etp/', datasetName, '/all_epochs/train/');
+    outputFolder = strcat(pwd, '/../datasets/open_source_d_etp/', datasetName, '/all_epochs/test/');
+    
+    files = dir(restIPIFolder);
 
-files = dir(restIPIFolder);
+    targetChannel = "Oz";
+    neighbors = ["O1", "O2", "Pz"];
+    targetFreq = [8 13];
 
-targetChannel = "Oz";
-neighbors = ["O1", "O2", "Pz"];
-targetFreq = [8 13];
+    for i = 1:length(files)
+        i
+        fileName = files(i).name;
 
-for i = 1:length(files)
-    i
-	fileName = files(i).name;
- 
-    if(~ endsWith(fileName, '.mat'))
-       continue; 
+        if(~ endsWith(fileName, '.mat'))
+           continue; 
+        end
+
+    %     Check if the matching file exists in the task set
+    %     Ex: PVT_S2_D4_REST_500ms_PHASES should correspond to PVT_S2_D4_TASK_DATA
+    
+        if(datasetName == "PVT")
+            taskFileName = strrep(fileName, 'REST_500ms_PHASES', 'TASK_DATA');
+        else
+            taskFileName = strrep(fileName, 'REST_PHASES', 'TASK_DATA');
+        end
+        
+        taskFilePath = strcat(taskFolder, taskFileName);
+
+        if(~ isfile(taskFilePath))
+            fprintf("%s has no matching task data\n", fileName);
+            continue;
+        end
+
+        restFilePath = strcat(restIPIFolder, fileName);
+        restIPIData = load(restFilePath).output;
+        taskEEG = load(taskFilePath);
+
+        electrodes = ExtractElectrodes(taskEEG.chanlocs, targetChannel, neighbors);
+        cycleEstimate = restIPIData.cycleEstimate;
+        cellData = ConvertToCellArray(taskEEG.data, 0);
+        [accuracies, allPhases, allPowers] = computeEpochAccuracy(cellData, 250, targetFreq, cycleEstimate, electrodes);
+
+        output = struct('accuracies', accuracies, 'allPhases', allPhases, 'allPowers', allPowers);
+
+        outputFileName = strrep(taskFileName, 'DATA', 'OUTPUT');
+        outputFilePath = strcat(outputFolder, outputFileName);
+
+        save(outputFilePath, 'output');
+
     end
     
-%     Check if the matching file exists in the task set
-%     Ex: PVT_S2_D4_REST_500ms_PHASES should correspond to PVT_S2_D4_TASK_DATA
-    taskFileName = strrep(fileName, 'REST_500ms_PHASES', 'TASK_DATA');
-    taskFilePath = strcat(taskFolder, taskFileName);
-    
-    if(~ isfile(taskFilePath))
-        fprintf("%s has no matching task data\n", fileName);
-        continue;
-    end
-    
-    restFilePath = strcat(restIPIFolder, fileName);
-    restIPIData = load(restFilePath).output;
-    taskEEG = load(taskFilePath);
-    
-    electrodes = ExtractElectrodes(taskEEG.chanlocs, targetChannel, neighbors);
-    cycleEstimate = restIPIData.cycleEstimate;
-    cellData = ConvertToCellArray(taskEEG.data, 0);
-    [accuracies, allPhases, allPowers] = computeEpochAccuracy(cellData, 250, targetFreq, cycleEstimate, electrodes);
-
-    output = struct('accuracies', accuracies, 'allPhases', allPhases, 'allPowers', allPowers);
-      
-    outputFileName = strrep(taskFileName, 'DATA', 'OUTPUT');
-    outputFilePath = strcat(outputFolder, outputFileName);
-
-    save(outputFilePath, 'output');
-
 end
--- ETPDrivers/ETPTrainDataset.m
+++ ETPDrivers/ETPTrainDataset.m
@@ -6,44 +6,54 @@ addpath(strcat(pwd, '/../ETPAlgorithm/dependencies/fieldtrip-20201214'));
 addpath(strcat(pwd, '/../ETPAlgorithm/utilities'));
 addpath(strcat(pwd, '/../ETPAlgorithm'));
 
-datasetName = 'PVT';
-inputFolder = strcat(pwd, '/../FinalDatasets/', datasetName, '/not_chan_reduced/pseudorest/500/mat/');
-outputFolder = strcat(pwd, '/../FinalDatasets/', datasetName, '/outputs/ETPTrain/pseudorest/500/');
+datasetNames = ["PVT" "ALPH" "B3" "COV" "AB"];
+restFolders = ["pseudorest" "rest" "rest" "rest" "rest"];
+timeIntervals = ["500/" "" "" "" ""];
 
-files = dir(inputFolder);
+for datasetIndex = 1:length(datasetNames)
+    datasetName = datasetNames(datasetIndex);
+    datasetRest = restFolders(datasetIndex);
+    timeInterval = timeIntervals(datasetIndex);
+    inputFolder = strcat(pwd, '/../datasets/open_source_c_epoched/', datasetName, '/not_chan_reduced/', datasetRest, '/', timeInterval, 'mat/');
+    outputFolder = strcat(pwd, '/../datasets/open_source_d_etp/', datasetName, '/all_epochs/train/');
 
-targetFreq = [8 13];
-targetChannel = "Oz";
-neighbors = ["O1", "O2", "Pz"];
+    files = dir(inputFolder);
 
-for i = 1:length(files)
-	fileName = files(i).name;
- 
-    if(~ endsWith(fileName, '.mat'))
-       continue; 
+    targetFreq = [8 13];
+    targetChannel = "Oz";
+    neighbors = ["O1", "O2", "Pz"];
+
+    for i = 1:length(files)
+        fileName = files(i).name;
+
+        if(~ endsWith(fileName, '.mat'))
+           continue; 
+        end
+        filePath = strcat(inputFolder, fileName);
+
+        EEG = load(filePath);
+
+        electrodes = ExtractElectrodes(EEG.chanlocs, targetChannel, neighbors);
+        % check if missing electrodes and skip
+        if(size(electrodes, 2) ~= size(neighbors, 2) + 1)
+            print(sprintf("%s missing electrodes", fileName));
+            continue;
+        end
+
+        originalData = EEG.data;
+        cellData = ConvertToCellArray(originalData, 0); % ETPTrainEpoch assumes data is in cell array (so that later we can have variable length epochs)
+
+        [cycleEstimate, actualPhases, actualPowers] = ETPTrainEpoch(cellData, 'TargetFreq', targetFreq, 'Electrodes', electrodes, 'SamplingRate', EEG.srate, ...
+                'Graph', false);
+
+        output = struct('cycleEstimate', cycleEstimate, 'actualPhases', actualPhases, 'actualPowers', actualPowers);
+
+        outputFileName = strrep(fileName, 'DATA', 'PHASES');
+        outputFilePath = strcat(outputFolder, outputFileName);
+
+        save(outputFilePath, 'output');
+
     end
-    filePath = strcat(inputFolder, fileName);
-    
-    EEG = load(filePath);
-
-    electrodes = ExtractElectrodes(EEG.chanlocs, targetChannel, neighbors);
-    % check if missing electrodes and skip
-    if(size(electrodes, 2) ~= size(neighbors, 2) + 1)
-        print(sprintf("%s missing electrodes", fileName));
-        continue;
-    end
-    
-    originalData = EEG.data;
-    cellData = ConvertToCellArray(originalData, 0); % ETPTrainEpoch assumes data is in cell array (so that later we can have variable length epochs)
-    
-    [cycleEstimate, actualPhases, actualPowers] = ETPTrainEpoch(cellData, 'TargetFreq', targetFreq, 'Electrodes', electrodes, 'SamplingRate', EEG.srate, ...
-            'Graph', false);
-	
-    output = struct('cycleEstimate', cycleEstimate, 'actualPhases', actualPhases, 'actualPowers', actualPowers);
-      
-    outputFileName = strrep(fileName, 'DATA', 'PHASES');
-    outputFilePath = strcat(outputFolder, outputFileName);
-
-    save(outputFilePath, 'output');
-
 end
+
+
