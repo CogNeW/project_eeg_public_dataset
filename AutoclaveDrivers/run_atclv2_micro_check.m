dbffd34b008970d9f39b50268f803525ee3a83ee
BE_Battlestation <be_battlestation@Brians-iMac.fios-router.home>
2022-02-08 18:20:51 -0500

    Add script for checking flags in micro

--- AutoclaveDrivers/run_atclv2_micro_check.m
+++ AutoclaveDrivers/run_atclv2_micro_check.m
@@ -6,7 +6,7 @@
 
 %% PREPROCESSING
 % Reads in parameters from a separate JSON file
-settings = jsondecode(fileread('dataset_common_settings.json'));
+settings = jsondecode(fileread('dataset_epoch_settings.json'));
 
 
 param = settings.MICRO.param_rest;
@@ -18,9 +18,24 @@ funct = {
     
 % cell of @functionHandles
 fullReport = atclv2_masterSelector(param,funct,...
-    'auto',0,'save',0,'vol',1,'global',0);
+    'auto',1,'save',0,'vol',1,'global',0);
 % auto - whether to ask what file to run
 % save - saves files during runtime
 % vol - 
 % global - 
+%%
+% Get a count of all the status
+statuses = [];
+for i = 1:size(fullReport, 1)
+    statuses = [statuses fullReport{i, 4}.status];
+end
 
+% -5 seems to be the most common, followed by -2 and -1
+% -999 appears twice
+
+count = 0;
+for i = 1:size(EEG.event, 2)
+   if(strcmp(EEG.event(i).type, 'S  1'))
+       count = count + 1;
+   end
+end
